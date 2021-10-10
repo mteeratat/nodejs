b0606087e931011ec54675d3f3f5404f39930ff4
M CPE <mmmiko@hotmail.com>
2021-10-10 22:19:11 +0700

    finish

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ let http = require('http');
 let dt = require('./myfirstmodule');
 let url = require('url');
 let fs = require('fs');
+let uc = require('upper-case');
+let rs = fs.createReadStream('./demo.txt');
+let events = require('events');
+let eventEmitter = new events.EventEmitter();
+
+// Create an event handler
+// let myEventHandler = function() {
+//     console.log('I hear scream');
+// }
+
+// Assign the event handler to an event
+// eventEmitter.on('scream', myEventHandler);
+
+// Fire the 'scream' event
+// eventEmitter.emit('scream');
+
+// rs.on('open', function() {
+//     console.log('The file is open');
+// })
 
 // let adr = 'http://localhost:8000/default.html?year=2020&month=november';
 // let q = url.parse(adr, true);
@@ -42,19 +61,31 @@ let fs = require('fs');
 //     })
 // }).listen(8000);
 
-http.createServer(function (req, res) {
-    let q = url.parse(req.url, true);
-    let filename = "." + q.pathname; // ./somepath
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end('404 Not Found');
-        }
-        res.writeHead(200, { 'Contetn-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
-}).listen(8000);
+// http.createServer(function (req, res) {
+//     let q = url.parse(req.url, true);
+//     let filename = "." + q.pathname; // ./somepath
+//     fs.readFile(filename, function(err, data) {
+//         if (err) {
+//             res.writeHead(404, { 'Content-Type': 'text/html' });
+//             return res.end('404 Not Found');
+//         }
+//         res.writeHead(200, { 'Contetn-Type': 'text/html' });
+//         res.write(data);
+//         return res.end();
+//     })
+// }).listen(8000);
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(uc.upperCase('teeratat pisarnsint'));
+//     res.end();
+// }).listen(8000);
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.write(uc.upperCase('teeratat pisarnsint'));
+//     res.end();
+// }).listen(8000);
 
 // fs.appendFile('mynewfile3.txt', 'This is update text', function(err) {
 //     if (err) throw err;
